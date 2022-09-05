4596ed7bead1e9fb89b52d93e965ea7dc8d8a1c3
Diego F. Aranha <dfaranha@gmail.com>
2022-09-05 15:29:03 +0100

    Add license, since it's derived from code available under the GPL3 in SUPERCOP.

--- x64/ffa.h
+++ x64/ffa.h
@@ -645,33 +645,497 @@ void low_inv_bas(__m128i *re_0, __m128i op00) {
 	return;
 }
 
+void _low_inv_gcd(__m128i *re_0, __m128i op00) {
+    uint64_t c0, c1, f[2], v[2];
+	int64_t delta = -1;
+    uint64_t g0 = 0x8000000000000001, g1 = 0x8000000000000000;
+	uint64_t u0 = 1, u1 = 0, v0 = 0, v1 = 0;
+
+	_mm_storeu_si128((__m128i *)f, op00);
+	uint64_t f0 = f[0];
+	uint64_t f1 = f[1];
+
+	for (int i = 0; i <= 2*127; i++) {
+        c0 = delta >> 63;
+        c1 = -(f0 & 1);
+
+		f0 ^= (g0 & c1);
+		f1 ^= (g1 & c1);
+
+		u0 ^= (v0 & c1);
+		u1 ^= (v1 & c1);
+
+		g0 ^= (f0 & c0);
+		g1 ^= (f1 & c0);
+
+		v0 ^= (u0 & c0);
+		v1 ^= (u1 & c0);
+
+        c0 &= c1;
+        delta = (delta ^ c0) - 1;
+
+		f0 = (f0 >> 1) | (f1 << 63);
+		f1 = (f1 >> 1);
+
+		uint64_t _u = u0 & 1;
+		u0 ^= _u;
+		u0 ^= _u << 63;
+		u1 ^= _u << 63;
+
+		u0 = (u0 >> 1) | (u1 << 63);
+		u1 = (u1 >> 1);
+	}
+
+    v[0] = v0;
+    v[1] = v1;
+	*re_0 = _mm_loadu_si128((__m128i *)v);
+	//gf_out(*re_0);
+}
+
+static inline ulong revbin(ulong x) {
+    int i;
+
+    union rev_t {
+        uint8_t bytes[8];
+        uint64_t value;
+    } rev;
+
+    rev.value = x;
+    for (i = 0; i < 8; i++)
+        rev.bytes[i] = (rev.bytes[i] * 0x0202020202ULL & 0x010884422010ULL) % 1023;
+    return __builtin_bswap64(rev.value);
+}
+
+void low_inv_gcd(__m128i *re_0, __m128i op00) {
+    uint64_t c[2] = {0x2000000000000000, 0x3000000000000000};
+    uint64_t f[2] = {0x8000000000000001, 0x8000000000000000};
+    uint64_t c0, c1, g[2], v[2];
+	uint64_t delta = 1, mask = (uint64_t)1 << 63;
+    uint64_t t1, t0, u0 = 1, u1 = 0, v0 = 0, v1 = 0;
+    uint64_t f0 = 0x1;
+	uint64_t f1 = 0x8000000000000001;
+
+    /* Store and fully reduce in the base field. */
+	_mm_storeu_si128((__m128i *)g, op00);
+    g[0] ^= g[1] & mask;
+    g[0] ^= g[1] >> 63;
+    g[1] ^= g[1] & mask;
+
+    uint64_t g0 = revbin(g[1]);
+	uint64_t g1 = revbin(g[0]);
+    g0 = (g0 >> 1) | (g1 << 63);
+    g1 = (g1 >> 1);
+
+	for (int i = 2*127 - 1; i > 0; i--) {
+        /* If delta > 0 && g0 & 1. */
+        c0 = (delta >> 63);
+        c1 = (g0 & 1);
+        c0 = (c0 ^ 1) & c1;
+
+        delta = (delta ^ -c0) + c0 + 1;
+
+        t0 = (f0 ^ g0) & -c0;
+        f0 ^= t0;
+        g0 ^= t0;
+        t1 = (f1 ^ g1) & -c0;
+        f1 ^= t1;
+        g1 ^= t1;
+
+        t0 = (u0 ^ v0) & -c0;
+        u0 ^= t0;
+        v0 ^= t0;
+        t1 = (u1 ^ v1) & -c0;
+        u1 ^= t1;
+        v1 ^= t1;
+
+        c0 = -(f0 & 1);
+        c1 = -(g0 & 1);
+
+        g0 = (g0 & c0) ^ (f0 & c1);
+        g1 = (g1 & c0) ^ (f1 & c1);
+        u0 = (u0 & c0) ^ (v0 & c1);
+        u1 = (u1 & c0) ^ (v1 & c1);
+
+        g0 = (g0 >> 1) | (g1 << 63);
+		g1 = (g1 >> 1);
+
+        v1 = (v1 << 1) | (v0 >> 63);
+        v0 = (v0 << 1);
+        v0 ^= v1 & mask;
+        v0 ^= v1 >> 63;
+        v1 ^= v1 & mask;
+	}
+
+    c0 = v0 & 1;
+    // Divite by x. */
+    v[0] = (v0 >> 1) | (v1 << 63);
+    v[1] = (v1 >> 1);
+    // Revert order and handle carry out of the shift above.
+    v1 = revbin(v[0]);
+	v0 = revbin(v[1]);
+    v[0] = (v0 >> 1) | (v1 << 63);
+    v[1] = (v1 >> 1);
+    v[0] ^= c0;
+    v[1] ^= c0;
+
+	*re_0 = _mm_loadu_si128((__m128i *)v);
+}
+
+void _low_inv_vec(__m128i *re_0, __m128i op00) {
+    uint64_t f0, d0, f[2], u[2] = {1,0}, v[2] = {0}, s[2] = {0x8000000000000001,0x8000000000000000};
+    int32_t delta = -1;
+
+    _mm_storeu_si128((__m128i *)f, op00);
+    __m128i _s = _mm_set_epi64x(s[1], s[0]);
+    __m128i _v = _mm_set_epi64x(v[1], v[0]);
+    __m128i _1 = _mm_set_epi64x(0x0, 0x1);
+    __m128i _2 = _mm_set_epi64x(0x1, 0x1);
+    __m128i _f = op00;
+    __m128i _u = _1;
+    __m128i _m = _mm_set_epi32(delta, delta, delta, delta);
+    __m128i _d0 = _m;
+
+    for (int i = 0; i <= 2*127; i++) {
+        __m128i _f0, t;
+
+        //_f0 = _mm_set_epi64x(-f0, -f0);
+        //STR((__m128i *)(f+0), _f);
+        t = _mm_and_si128(_f, _1);
+        t = _mm_xor_si128(t, _mm_slli_si128(t, 8));
+        _f0 = _mm_cmpeq_epi64(t, _2);
+        //f0 = f[0] & 1; d0 = delta >> 31;
+
+        //_d0 = _mm_set_epi32(delta, delta, delta, delta);
+        t = _mm_srai_epi32(_d0, 31);
+        //printf("%X\n", d0);
+        //uint32_t _t[4];
+        //STR((__m128i *)(_t+0), _d0);
+        //printf("%X %X\n", t[0], t[1]);
+        //exit(0);
+
+        //f[0] ^= (s[0] & -f0);
+        //f[1] ^= (s[1] & -f0);
+        _f = _mm_xor_si128(_f, _mm_and_si128(_s, _f0));
+
+        //u[0] ^= (v[0] & -f0);
+        //u[1] ^= (v[1] & -f0);
+        _u = _mm_xor_si128(_u, _mm_and_si128(_v, _f0));
+
+        //s[0] ^= (f[0] & d0);
+        //s[1] ^= (f[1] & d0);
+        _s = _mm_xor_si128(_s, _mm_and_si128(_f, t));
+
+        //v[0] ^= (u[0] & d0);
+        //v[1] ^= (u[1] & d0);
+        _v = _mm_xor_si128(_v, _mm_and_si128(_u, t));
+
+        //printf("%d\n", delta);
+        //STR((__m128i *)(_t+0), _d0);
+        //printf("%d %d %d %d\n", _t[3], _t[2], _t[1], _t[0]);
+        //delta = SELECT(delta, -delta, f0 & -d0);
+        //_d0 = _mm_sign_epi32(_d0, _mm_and_si128(_f0, t));
+        _d0 = _mm_blendv_epi8(_d0, _mm_sign_epi32(_d0, _m), _mm_and_si128(_f0, t));
+        //printf("%d\n", delta);
+        //STR((__m128i *)(_t+0), _d0);
+        //printf("%d %d %d %d\n", _t[3], _t[2], _t[1], _t[0]);
+
+        //STR((__m128i *)(u+0), _u);
+
+        //f[0] = (f[0] >> 1) | (f[1] << 63);
+        //f[1] = (f[1] >> 1);
+        _f = _mm_xor_si128(_mm_srli_epi64(_f, 1), _mm_slli_epi64(_mm_srli_si128(_f, 8), 63));
+
+        //uint64_t u0 = u[0] & 1;
+        //u[0] ^= u0;
+        //u[0] ^= u0 << 63;
+        //u[1] ^= u0 << 63;
+        t = _mm_and_si128(_u, _1);
+        _u = _mm_xor_si128(_u, t);
+        t = _mm_slli_epi64(t, 63);
+        _u = _mm_xor_si128(_u, t);
+        t = _mm_slli_si128(t, 8);
+        _u = _mm_xor_si128(_u, t);
+
+        //u[0] = (u[0] >> 1) | (u[1] << 63);
+        //u[1] = (u[1] >> 1);
+        _u = _mm_xor_si128(_mm_srli_epi64(_u, 1), _mm_slli_epi64(_mm_srli_si128(_u, 8), 63));
+
+        //delta--;
+        _d0 = _mm_add_epi32(_d0, _m);
+    }
+
+    *re_0 = _v;//_mm_loadu_si128((__m128i *)v);
+    //gf_out(*re_0);
+}
+
+#define CTIME_IF(mask,then,else)  ((mask&(then)) | (~mask&(else) ))
+
+#define CSWAP(U, V, T, C)      (T = ((U) ^ (V)) & -C, U ^= T, V^= T)
+
+static int divstepsx(int n, uint64_t delta, uint64_t f64, uint64_t g64, uint64_t * p00, uint64_t * p01, uint64_t * p10, uint64_t * p11) {
+    uint64_t u, v, q, r;
+    uint64_t g0, f0;
+
+    u = ((uint64_t) 1) << n;
+    v = 0;
+    q = 0;
+    r = ((uint64_t) 1) << n;
+    uint64_t tmp,tmp2;
+
+    while (n > 0) {
+        int64_t swap_mask = ((delta > 0) & ((g64 & 1) != 0));
+        swap_mask = (swap_mask << (8-1)) >> (8-1);
+        delta = (delta ^ -swap_mask) + swap_mask;
+        //delta = CTIME_IF(swap_mask,-delta,delta);
+        CSWAP(f64, g64, tmp, swap_mask);
+        CSWAP(q, u, tmp, swap_mask);
+        CSWAP(r, v, tmp, swap_mask);
+
+        delta++;
+        g0 = (uint64_t)0 - (g64 & (uint64_t) 0x1);
+        f0 = (uint64_t)0 - (f64 & (uint64_t) 0x1);
+
+        q =   (f0 & q) ^ (g0 & u);
+        r =   (f0 & r) ^ (g0 & v);
+        g64 = (f0 & g64) ^ (g0 & f64);
+        g64 >>= 1;
+        q >>= 1;
+        r >>= 1;
+        n--;
+    } //end while
+    *p00 = u;
+    *p01 = v;
+    *p10 = q;
+    *p11 = r;
+
+    return delta;
+}
+
+
+static inline __m128i right_shift_128(__m128i in) {
+
+    __m128i a,b;
+    a = _mm_srli_epi64(in,1);
+    b = _mm_slli_epi64(in,8-1);
+
+    //set the high part of b = 0
+    b = _mm_unpacklo_epi64( _mm_setzero_si128(),b ); //o _mm_unpacklo_epi64???
+    a = _mm_or_si128(a,b);
+
+    return a;
+}
+
+#define CTIME_IF_128(mask,then,else)  _mm_or_si128(_mm_and_si128(mask, then) ,_mm_andnot_si128(mask, else))
+
+static inline int divstepsx_128(int n, int delta, uint64_t f[], uint64_t g[], uint64_t *p00, uint64_t *p01, uint64_t *p10, uint64_t *p11) {
+
+    if(n<64)
+        return delta = divstepsx (n, delta, f[0], g[0], p00, p01, p10, p11);
+
+    __m128i g0, f0, g128, f128;
+    __m128i one_128 = _mm_set_epi64x((uint64_t) 1, (uint64_t) 0);
+    __m128i mask_128 = _mm_set_epi64x((uint64_t) 1, (uint64_t) 1);
+    __m128i zero_128 = _mm_setzero_si128();
+
+    __m128i u, v, q, r;
+
+    g128 = _mm_lddqu_si128((__m128i *)g);
+    f128 = _mm_lddqu_si128((__m128i *)f);
+
+    uint64_t  temp = ((uint64_t) 1)<< (n-64);
+    u = _mm_set_epi64x( (uint64_t) 0, temp);
+    r = _mm_set_epi64x( (uint64_t) 0, temp);
+    v = _mm_setzero_si128();
+    q = _mm_setzero_si128();
+
+    __m128i delta_128 = _mm_set_epi64x((int64_t) delta,(int64_t) delta);
+
+    __m128i tmp,tmp2;
+
+    while (n > 0) {
+        __m128i delta_mask = _mm_cmpgt_epi64(delta_128, zero_128);
+
+        //something like [xxx....xxx | FFF....FFF] where x is the actual mask
+        __m128i g128_mask = _mm_cmpeq_epi64(_mm_and_si128(g128, one_128), one_128);
+
+        __m128i swap_mask = _mm_and_si128(delta_mask, (__m128i)_mm_shuffle_pd((__m128d) g128_mask, (__m128d) g128_mask, 3));
+
+
+        delta_128 = _mm_add_epi64(_mm_xor_si128(delta_128, swap_mask), _mm_and_si128(mask_128, swap_mask));
+
+        // delta = CTIME_IF(swap,-delta,delta);
+        tmp  = CTIME_IF_128(swap_mask,g128,f128);
+        tmp2 = CTIME_IF_128(swap_mask,f128,g128);
+
+
+        f128  = tmp;
+        g128  = tmp2;
+
+        tmp  = CTIME_IF_128(swap_mask,q,u);
+        tmp2 = CTIME_IF_128(swap_mask,u,q);
+
+
+        u  = tmp;
+        q  = tmp2;
+
+        tmp  = CTIME_IF_128(swap_mask,r,v);
+        tmp2 = CTIME_IF_128(swap_mask,v,r);
+
+        v  = tmp;
+        r  = tmp2;
+
+        //delta++;
+        delta_128 = _mm_add_epi64(delta_128, mask_128);
+
+        g0 = _mm_cmpeq_epi64(_mm_and_si128(g128, one_128), one_128);
+        g0 = (__m128i)_mm_shuffle_pd((__m128d) g0, (__m128d) g0, 3);
+
+        f0 = _mm_cmpeq_epi64(_mm_and_si128(f128, one_128), one_128);
+        f0 = (__m128i)_mm_shuffle_pd((__m128d) f0, (__m128d) f0, 3);
+
+        q =   _mm_xor_si128(_mm_and_si128(f0,q), _mm_and_si128(g0,u)); //(f0 & q) ^ (g0 & u);
+        r =   _mm_xor_si128(_mm_and_si128(f0,r), _mm_and_si128(g0,v)); //(f0 & r) ^ (g0 & v);
+        g128 = _mm_xor_si128(_mm_and_si128(f0,g128), _mm_and_si128(g0,f128)); //(f0 & g64) ^ (g0 & f64);
+
+        g128 = right_shift_128(g128);
+        q = right_shift_128(q);
+        r = right_shift_128(r);
+        n--;
+    } //end while
+
+    _mm_storeu_si128((__m128i *) p00, u);
+    _mm_storeu_si128((__m128i *) p01, v);
+    _mm_storeu_si128((__m128i *) p10, q);
+    _mm_storeu_si128((__m128i *) p11, r);
+
+    return 0;//_mm_cvtsi128_si64x(delta_128);//_mm_extract_epi64(delta_128,1);
+}
+
+static inline __m256i right_shift_256(__m256i in) {
+    __m256i a,b,c;
+    a = _mm256_srli_epi64(in,1);
+
+    b = _mm256_slli_epi64(in,8-1);
+
+    c = _mm256_permute4x64_epi64(b,0x93);
+    c = _mm256_insert_epi64(c, (uint64_t) 0, 0);
+
+    a = _mm256_or_si256(a,c);
+
+    return a;
+}
+
+#define CTIME_IF_256(mask,then,else)  _mm256_blendv_epi8(else, then, mask)
+
+static inline int divstepsx_256(int n, int delta, uint64_t f[], uint64_t g[], uint64_t *p00, uint64_t *p01, uint64_t *p10, uint64_t *p11) {
+
+    if(n<128)
+        return delta = divstepsx_128 (n,delta, f, g, p00, p01, p10, p11);
+
+    __m256i g0, f0, g256, f256;
+    __m256i one_256 = _mm256_set_epi64x((uint64_t) 1, (uint64_t) 0,(uint64_t) 0, (uint64_t) 0);
+    __m256i mask_256 = _mm256_set_epi64x((uint64_t) 1, (uint64_t) 1,(uint64_t) 1, (uint64_t) 1);
+    __m256i zero_256 = _mm256_setzero_si256();
+
+
+    __m256i u, v, q, r;
+
+    g256 = _mm256_lddqu_si256((__m256i *)g);
+    f256 = _mm256_lddqu_si256((__m256i *)f);
+
+    uint64_t  temp = ((uint64_t) 1)<< (n-192);
+    u = _mm256_set_epi64x( (uint64_t) 0, (uint64_t) 0, (uint64_t) 0, temp);
+    r = _mm256_set_epi64x( (uint64_t) 0, (uint64_t) 0, (uint64_t) 0, temp);
+    v = _mm256_setzero_si256();
+    q = _mm256_setzero_si256();
+
+    __m256i delta_256 = _mm256_set_epi64x((int64_t) delta,(int64_t) delta, (int64_t) delta,(int64_t) delta);
+
+    __m256i tmp,tmp2;
+
+    while (n > 0) {
+        __m256i delta_mask = _mm256_cmpgt_epi64(delta_256, zero_256);
+        __m256i g256_mask = _mm256_cmpeq_epi64(_mm256_and_si256(g256, one_256), one_256);
+
+        __m256i swap_mask = _mm256_and_si256(delta_mask, _mm256_permute4x64_epi64(g256_mask, 0xFF));
+
+
+        //need to add 1 when changing sign with the xor
+        delta_256 = _mm256_add_epi64(_mm256_xor_si256(delta_256, swap_mask), _mm256_and_si256(mask_256, swap_mask));
+        tmp  = CTIME_IF_256(swap_mask,g256,f256);
+        tmp2 = CTIME_IF_256(swap_mask,f256,g256);
+
+        f256  = tmp;
+        g256  = tmp2;
+
+        tmp  = CTIME_IF_256(swap_mask,q,u);
+        tmp2 = CTIME_IF_256(swap_mask,u,q);
+
+
+        u  = tmp;
+        q  = tmp2;
+
+        tmp  = CTIME_IF_256(swap_mask,r,v);
+        tmp2 = CTIME_IF_256(swap_mask,v,r);
+
+        v  = tmp;
+        r  = tmp2;
+
+        delta_256 = _mm256_add_epi64(delta_256, mask_256);
+
+        __m256i maskgf_tmp = _mm256_cmpeq_epi64(_mm256_and_si256(g256, one_256), one_256);
+        g0 = _mm256_permute4x64_epi64(maskgf_tmp, 0xFF);
+
+        maskgf_tmp = _mm256_cmpeq_epi64(_mm256_and_si256(f256, one_256), one_256);
+        f0 = _mm256_permute4x64_epi64(maskgf_tmp, 0xFF);
+
+        q =   _mm256_xor_si256(_mm256_and_si256(f0,q), _mm256_and_si256(g0,u));
+        r =   _mm256_xor_si256(_mm256_and_si256(f0,r), _mm256_and_si256(g0,v));
+        g256 = _mm256_xor_si256(_mm256_and_si256(f0,g256), _mm256_and_si256(g0,f256));
+
+        g256 = right_shift_256(g256);
+        q = right_shift_256(q);
+        r = right_shift_256(r);
+        n--;
+
+    } //end while
+
+    _mm256_storeu_si256((__m256i *) p00, u);
+    _mm256_storeu_si256((__m256i *) p01, v);
+    _mm256_storeu_si256((__m256i *) p10, q);
+    _mm256_storeu_si256((__m256i *) p11, r);
+
+    return _mm256_extract_epi64(delta_256, 0);
+}
+
 /* [F_q^2] inversion */
 void low_inv(__m128i *re_0, __m128i *re_1, __m128i op00, __m128i op01) {
-	/* var */
-	__m128i are, aim, cre, cim;
-	__m128i t00, t01, t02;
+    /* var */
+    __m128i are, aim, cre, cim;
+    __m128i t00, t01, t02;
 
-	/* inversion: pre */
-	are = _mm_unpacklo_epi64(op00, op01);
-	aim = _mm_unpackhi_epi64(op00, op01);
+    /* inversion: pre */
+    //low_red_127_063_000(op00, op01, t00);
+    are = _mm_unpacklo_epi64(op00, op01);
+    aim = _mm_unpackhi_epi64(op00, op01);
 
-	/* inversion */
-	t00 = _mm_xor_si128(are, aim);	/* t00 = a_r + a_i */
-	low_mul_bas(&t01, are, aim);	/* t01 = a_r * a_i */
-	low_sqr_bas(&t02, t00);		/* t02 = (a_r + a_i)^2 */
-	t01 = _mm_xor_si128(t01, t02);	/* t = a_r * a_i + (a_r + a_i)^2 */
+    /* inversion */
+    t00 = _mm_xor_si128(are, aim);  /* t00 = a_r + a_i */
+    low_mul_bas(&t01, are, aim);    /* t01 = a_r * a_i */
+    low_sqr_bas(&t02, t00);         /* t02 = (a_r + a_i)^2 */
+    t01 = _mm_xor_si128(t01, t02);  /* t = a_r * a_i + (a_r + a_i)^2 */
 
-	low_inv_bas(&t01, t01);		/* t = t^-1 */
+    low_inv_bas(&t01, t01);         /* t = t^-1 */
 
-	low_mul_bas(&cre, t00, t01);	/* c_r = (a_r + a_i) * t */
-	low_mul_bas(&cim, aim, t01);	/* c_i = a_i * t */
+    low_mul_bas(&cre, t00, t01);    /* c_r = (a_r + a_i) * t */
+    low_mul_bas(&cim, aim, t01);    /* c_i = a_i * t */
 
-	/* inversion: post */
-	*re_0 = _mm_unpacklo_epi64(cre, cim);
-	*re_1 = _mm_unpackhi_epi64(cre, cim);
+    /* inversion: post */
+    *re_0 = _mm_unpacklo_epi64(cre, cim);
+    *re_1 = _mm_unpackhi_epi64(cre, cim);
 
-	/* end */
-	return;
+    /* end */
+    return;
 }
 
 void low_inv_sim(__m128i *o0, __m128i *o1, __m128i *i0, __m128i *i1, uint64_t len, int ct) {
